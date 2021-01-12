1adf8399e220b88e0450746b7622adb3f8cdd5e9
FelcyJenolin-J <felcyjenolin21@gmail.com>
2021-01-12 11:05:17 +0530

    changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,177 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData) {
+            return LogOut();
+          } else if (userSnapshot.hasError) {
+            return CircularProgressIndicator();
+          }
+          return SignInScreen();
+        },
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SignInScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _SignInScreenState createState() => _SignInScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _SignInScreenState extends State<SignInScreen> {
+  String message;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Image.network(
+              "https://www.3nions.com/wp-content/uploads/2020/01/comp_1.gif",
+              height: 200,
+            ),
+            InkWell(
+              onTap: () {
+                _login();
+              },
+              borderRadius: BorderRadius.circular(30),
+              splashColor: Colors.blue,
+              child: Container(
+                height: 50,
+                width: 300,
+                child: Center(
+                    child: Text(
+                  "Sign In using twitter",
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(
+                      color: Colors.blue,
+                      width: 3,
+                    )),
+              ),
+            ),
+            SizedBox(
+              height: 20,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              message == null ? "" : message,
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _login() async {
+    final TwitterLoginResult result = await twitterLogin.authorize();
+    String newMessage;
+    if (result.status == TwitterLoginStatus.loggedIn) {
+      _signInWithTwitter(result.session.token, result.session.secret);
+    } else if (result.status == TwitterLoginStatus.cancelledByUser) {
+      newMessage = 'Login cancelled by user.';
+    } else {
+      newMessage = result.errorMessage;
+    }
+
+    setState(() {
+      message = newMessage;
+    });
+  }
+
+  void _signInWithTwitter(String token, String secret) async {
+    final AuthCredential credential = TwitterAuthProvider.getCredential(
+        authToken: token, authTokenSecret: secret);
+    await _auth.signInWithCredential(credential);
+  }
+}
+
+FirebaseAuth _auth = FirebaseAuth.instance;
+final TwitterLogin twitterLogin = new TwitterLogin(
+  consumerKey: '',
+  consumerSecret: '',
+);
+
+void _logout() async {
+  await twitterLogin.logOut();
+  await _auth.signOut();
+}
+
+class LogOut extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Twitter Auth Demo"),
+      ),
+      body: FutureBuilder(
+        future: FirebaseAuth.instance.currentUser(),
+        builder: (context, snapshot) {
+          FirebaseUser firebaseUser = snapshot.data;
+          return snapshot.hasData
+              ? Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "SignIn Success 😊",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text("UserId: ${firebaseUser.uid}"),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Image.network(
+                        firebaseUser.photoUrl,
+                        height: 100,
+                      ),
+                      Text("Your name: ${firebaseUser.displayName}"),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      RaisedButton(
+                        onPressed: _logout,
+                        child: Text(
+                          "LogOut",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.blue,
+                      )
+                    ],
+                  ),
+                )
+              : CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
